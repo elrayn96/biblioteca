6596cc75826349f13ee7aa048f17d943acd0232a
elrayn96 <elrayn96@gmail.com>
2025-07-05 06:41:33 +0200

    Create AutorRepository.java

---
+++ src/main/java/com/armando/biblioteca/repository/AutorRepository.java
@@ -0,0 +1,19 @@
+package com.armando.biblioteca.repository;
+
+
+
+import com.armando.biblioteca.model.Autor;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.springframework.stereotype.Repository;
+
+
+
+@Repository
+
+public interface AutorRepository extends  JpaRepository<Autor, String> {
+
+    boolean existsById(String id);
+
+}
